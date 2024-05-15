346fa57ef92033e490ca69944e52c1d8e5db4650
Eric Eberhart <el.eberhart40@gmail.com>
2024-05-15 09:31:55 -0400

    feat: fetch and group jobs by employer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,60 @@
+import { EmployerCardProps } from "@/components/EmployerCard";
+import type { Job } from "./types/Job";
+import { EmployerJobList } from "../components/EmployerJobList";
 
-import {Logo} from "@/components/Logo"
+const groupJobsByEmployer = (jobs: Job[]): EmployerCardProps[] => {
+  const employerMap: { [key: string]: EmployerCardProps } = {};
 
-export default function Home() {
+  jobs.forEach((job) => {
+    const {
+      employer_title,
+      employer_logo_url,
+      urgent_hiring,
+      id,
+      title,
+      payMin,
+      payMax,
+    } = job;
+
+    if (!employerMap[employer_title]) {
+      employerMap[employer_title] = {
+        employer_title,
+        employer_logo_url,
+        urgent_hiring,
+        jobs: [],
+      };
+    }
+
+    employerMap[employer_title].jobs.push({ id, title, payMin, payMax });
+  });
+
+  return Object.values(employerMap);
+};
+
+const fetchJobs: () => Promise<Job[]> = async () => {
+  const response = await fetch("http://localhost:3000/api/jobs");
+  const data = await response.json();
+  return data.data.filter((job: { verified: boolean }) => job.verified);
+};
+
+export default async function Home() {
+  const jobs = await fetchJobs();
+  const groupedJobs = groupJobsByEmployer(jobs);
+
+  const [totalJobs, totalEmployers] = groupedJobs.reduce(
+    (acc, group) => {
+      acc[0] += group.jobs.length;
+      acc[1] += 1;
+      return acc;
+    },
+    [0, 0]
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Logo />
-        <p>Build components here</p>
-      </div>
-    </main>
+    <EmployerJobList
+      groupedJobs={groupedJobs}
+      totalJobs={totalJobs}
+      totalEmployers={totalEmployers}
+    />
   );
 }
