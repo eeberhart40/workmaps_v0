4a4742978fe680fb5941f4d0c3750a0980f37427
Eric Eberhart <el.eberhart40@gmail.com>
2024-05-15 09:15:50 -0400

    chore(config): add image domain config to nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    rewrites: async () => [
-      // These routes redirect the user. API routes redirect faster than server
-      // components, but must live in the `api` directory. These rewrites allow us
-      // to hide the `/api` portion of the URL while keeping the redirect fast.
-      {
-        source: "/click-out/:jobID*",
-        destination: "/api/click-out/:jobID*",
-      },
-    ],
-  }
+  rewrites: async () => [
+    // These routes redirect the user. API routes redirect faster than server
+    // components, but must live in the `api` directory. These rewrites allow us
+    // to hide the `/api` portion of the URL while keeping the redirect fast.
+    {
+      source: "/click-out/:jobID*",
+      destination: "/api/click-out/:jobID*",
+    },
+  ],
+  images: {
+    domains: ["imagedelivery.net"],
+  },
+};
 
 export default nextConfig;
